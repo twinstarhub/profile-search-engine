04ad5b6d6bf8586cb3e520a5c0e4dfbf7905cd13
twinstarhub <golddev9697@gmail.com>
2023-07-31 16:37:43 +0900

    Measuring sentence_transformer and pytorch cosine similarity

--- apps/googleapi/serpscrap.py
+++ apps/googleapi/serpscrap.py
@@ -56,3 +56,45 @@ class Serpscrap():
             search_results.append(search_result)
 
         return search_results
+    
+     def scrap_youtube(self, query):
+        """Scrap YouTube search results"""
+        # Assuming you have a method in your ApifyAPI for YouTube scraping
+        youtube_search_results = ApifyAPI.scrap_youtube_results(query)
+        return youtube_search_results
+
+    def scrap_facebook(self, query):
+        """Scrap Facebook search results"""
+        # Assuming you have a method in your ApifyAPI for Facebook scraping
+        facebook_search_results = ApifyAPI.scrap_facebook_results(query)
+        return facebook_search_results
+
+    def scrap_twitter(self, query):
+        """Scrap Twitter search results"""
+        # Assuming you have a method in your ApifyAPI for Twitter scraping
+        twitter_search_results = ApifyAPI.scrap_twitter_results(query)
+        return twitter_search_results
+
+    def scrap_tiktok(self, query):
+        """Scrap TikTok search results"""
+        # Assuming you have a method in your ApifyAPI for TikTok scraping
+        tiktok_search_results = ApifyAPI.scrap_tiktok_results(query)
+        return tiktok_search_results
+
+    def integrate_all_search_results(self, all_usernames):
+        # Initialize a dictionary to store the search results for each username
+        all_search_results = {}
+
+        # Loop through each username and scrape search results from all platforms
+        for username in all_usernames:
+            search_results = {}
+            search_results['Google'] = self.scrap_google_search_result(username)
+            search_results['Instagram'] = self.scrap_insta_offloading(username)
+            search_results['YouTube'] = self.scrap_youtube(username)
+            search_results['Facebook'] = self.scrap_facebook(username)
+            search_results['Twitter'] = self.scrap_twitter(username)
+            search_results['TikTok'] = self.scrap_tiktok(username)
+
+            all_search_results[username] = search_results
+
+        return all_search_results
